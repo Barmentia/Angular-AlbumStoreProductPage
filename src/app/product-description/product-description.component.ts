04b16caea06928d3bf437c764fd483c9358bfb3b
Beni de Armentia <barmentia@gmail.com>
2019-04-04 21:25:48 +0100

    Creating a Service to get album data

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,12 +8,13 @@ import { ProductService } from 'app/product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  albumInfo: any;
+  albumInfo;
 
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
+    this._productService.getAlbum(1).subscribe(response => 
+      this.albumInfo = response);
   }
 
 }
